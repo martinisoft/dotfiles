579831acc2003a5cdc4193a807794877231971a1
Aaron Kalin <dev+martinisoft@martinisoftware.com>
2012-04-22 21:59:16 -0500

    Add project centric tmux command

--- .zsh/plugins/projects.plugin.zsh
+++ .zsh/plugins/projects.plugin.zsh
@@ -1,2 +1,21 @@
 pro() { cd ~/projects/$1; }
-compctl -W ~/projects -/ pro
+
+# Sourced from https://github.com/hashrocket/dotmatrix
+pux() {
+  local name cols
+  if [ -n "$1" ]; then
+    pro $1
+  fi
+  name="$(basename $PWD | sed -e 's/\./-/g')"
+  cols="$(tput cols)"
+  if ! $(tmux has-session -t $name &>/dev/null); then
+    tmux new-session -d -n code -s $name -x${cols-150} -y50
+    tmux split-window -t $name:0
+    tmux new-window -a -d -n server -t $name:0
+    tmux select-layout -t $name main-vertical
+    tmux send-keys -t $name:0.1 'vim .' C-m
+  fi
+  tmux attach -t $name
+}
+
+compctl -W ~/projects -/ pro pux
