a4f061115de82072e7adbe15019df32f8b67b9b8
Aaron Kalin <akalin@martinisoftware.com>
2012-04-28 23:48:17 -0500

    Add console window for pux

--- .zsh/plugins/projects.plugin.zsh
+++ .zsh/plugins/projects.plugin.zsh
@@ -12,6 +12,7 @@ pux() {
     tmux new-session -d -n code -s $name -x${cols-150} -y50
     tmux split-window -t $name:0
     tmux new-window -a -d -n server -t $name:0
+    tmux new-window -a -d -n console -t $name:1
     tmux select-layout -t $name main-vertical
     tmux send-keys -t $name:0.1 'vim .' C-m
   fi
