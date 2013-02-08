18c56abadab44af742a87a48050cf8fc341c847d
Aaron Kalin <akalin@martinisoftware.com>
2013-02-08 15:50:18 +0200

    Tweak pux function

--- .zsh/plugins/projects.plugin.zsh
+++ .zsh/plugins/projects.plugin.zsh
@@ -9,12 +9,11 @@ pux() {
   name="$(basename $PWD | sed -e 's/\./-/g')"
   cols="$(tput cols)"
   if ! $(tmux has-session -t $name &>/dev/null); then
-    tmux new-session -d -n code -s $name -x${cols-150} -y50
-    tmux split-window -t $name:1
+    tmux new-session -d -n code -s $name
     tmux new-window -a -d -n server -t $name:1
     tmux new-window -a -d -n console -t $name:2
     tmux select-layout -t $name main-vertical
-    tmux send-keys -t $name:1.1 'vim .' C-m
+    tmux send-keys -t $name:1 'vim .' C-m
   fi
   tmux attach -t $name
 }
