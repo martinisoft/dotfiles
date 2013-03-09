19fb418a3136f0d2e6417799b7eeb2fffecff4e2
Aaron Kalin <akalin@martinisoftware.com>
2013-03-09 14:28:12 -0800

    Stop guard from running the suite on green
    By default Guard will attempt to run the entire suite again when you get
    a passing test. With larger, longer suites this becomes quite painful

--- .guard.rb
+++ .guard.rb
@@ -9,3 +9,5 @@ notification :tmux,
   # *pending_message_format*, *failed_message_format*
   line_separator: ' > ', # since we are single line we need a separator
   color_location: 'status-left-bg' # to customize which tmux element will change color
+
+guard 'rspec', all_after_pass: false
