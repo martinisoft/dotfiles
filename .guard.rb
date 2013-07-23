af91e8b11e37a6bb63c37c38d47e05cc25fc9dcf
Aaron Kalin <akalin@martinisoftware.com>
2013-07-22 20:56:11 -0500

    Can't do this globally

--- .guard.rb
+++ .guard.rb
@@ -9,5 +9,3 @@ notification :tmux,
   # *pending_message_format*, *failed_message_format*
   line_separator: ' > ', # since we are single line we need a separator
   color_location: 'status-left-bg' # to customize which tmux element will change color
-
-guard 'rspec', all_after_pass: false
