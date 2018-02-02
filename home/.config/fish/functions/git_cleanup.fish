4ec4525e472d42d4151334db0c0a0e1d8b8913a3
Aaron Kalin <akalin@martinisoftware.com>
2018-02-01 21:14:29 -0600

    Bugfix: Use POSIX space char class instead
    The \t got greedy on accident and proabbly has to do with escaping so
    switching to good ol char classes instead

--- home/.config/fish/functions/git_cleanup.fish
+++ home/.config/fish/functions/git_cleanup.fish
@@ -1,5 +1,5 @@
 function git_cleanup
-  for branch in (git branch -vv | grep ' gone' | cut -d' ' -f1-3 | sed -e 's/^[ \t]*//')
+  for branch in (git branch -vv | grep ' gone' | cut -d' ' -f1-3 | sed -e 's/^[[:space:]]*//')
     if user_prompt "Remove deleted branch $branch?"
       git branch -D $branch
     end
