62bd200ba8b6a85cb65faaaf9abb895e39aff586
Aaron Kalin <akalin@martinisoftware.com>
2019-06-11 11:25:59 -0500

    Re-add GOPATH because we do need it for tooling
    Will still override it everywhere until GO 1.13 comes out

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,6 +3,7 @@ set -g -x PATH ~/.cargo/bin $PATH
 set -g -x PATH /usr/local/bin $PATH
 set -g -x GPG_TTY (tty)
 set -x EDITOR nvim
+set -g -x GOPATH ~/.go
 set -g -x PATH ~/.go/bin $PATH
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
 set fish_greeting
