7d7590c392c6eaae0ed275623bb12a5e705b1414
Aaron Kalin <akalin@martinisoftware.com>
2019-09-29 15:50:00 -0500

    Fix python tool pathing
    This lets me use python tools that are universal like pg formatter
    within my PATH without having to resort to lots of fancy virtualenv
    tricks

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -5,6 +5,7 @@ set -g -x GPG_TTY (tty)
 set -x EDITOR nvim
 set -g -x GOPATH ~/.go
 set -g -x PATH ~/.go/bin $PATH
+set -g -x PATH (python3 -c 'import site; print(site.USER_BASE)')/bin $PATH
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
 source "$HOME/.cargo/env"
 set fish_greeting
